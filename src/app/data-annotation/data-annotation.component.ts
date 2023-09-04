9587db4bdf6bdc933e79ab80875c8c3da192b020
Rahulraja G <rr7537007@gmail.com>
2023-09-04 18:48:39 +0530

    Project is added to github

---
+++ src/app/data-annotation/data-annotation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-annotation',
+  templateUrl: './data-annotation.component.html',
+  styleUrls: ['./data-annotation.component.css']
+})
+export class DataAnnotationComponent {
+
+}
